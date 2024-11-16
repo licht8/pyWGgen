fc86b6388f0c5adce2e747893dfcc8b97b5b5f3c
Yehor <116878096+licht8@users.noreply.github.com>
2024-11-16 16:53:29 +0100

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,20 @@
+# settings.py
+
+import os
+
+# Пути к файлам и директориям
+BASE_DIR = "user/data"
+WG_CONFIG_DIR = os.path.join(BASE_DIR, "wg_configs")
+QR_CODE_DIR = os.path.join(BASE_DIR, "qrcodes")
+STALE_CONFIG_DIR = os.path.join(BASE_DIR, "usr_stale_config")
+USER_DB_PATH = os.path.join(BASE_DIR, "user_records.json")
+IP_DB_PATH = os.path.join(BASE_DIR, "ip_records.json")
+SERVER_CONFIG_FILE = "/etc/wireguard/wg0.conf"  # Путь к конфигурационному файлу сервера WireGuard
+PARAMS_FILE = "/etc/wireguard/params"           # Путь к файлу с параметрами WireGuard
+
+# Параметры WireGuard
+DEFAULT_TRIAL_DAYS = 30                          # Базовый срок действия аккаунта в днях
+
+# Настройки для логирования (если используем модуль логирования)
+LOG_FILE_PATH = os.path.join(BASE_DIR, "logs", "app.log")
+LOG_LEVEL = "INFO"
