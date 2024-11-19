8d4ea6bbf8a0c9d8ad95e07f67be95a4caf3e489
Edd <33794654+giteed@users.noreply.github.com>
2024-11-19 05:06:58 +0300

    Update ip_management.py

--- modules/ip_management.py
+++ modules/ip_management.py
@@ -29,31 +29,14 @@ def save_ip_records(ip_records):
     with open(settings.IP_DB_PATH, 'w') as file:
         json.dump(ip_records, file, indent=4)
 
-def generate_ip(existing_ips):
-    """Генерирует новый IP-адрес или использует освобожденный, если доступен"""
-    ip_records = load_ip_records()
-    
-    # Используем освобожденные IP, если они есть
-    free_ips = [ip for ip, in_use in ip_records.items() if not in_use]
-    if free_ips:
-        new_ip = free_ips.pop(0)
-        ip_records[new_ip] = True  # Помечаем IP как используемый
-        save_ip_records(ip_records)
-        return f"{new_ip}/32", new_ip
+def generate_ip(existing_ips, subnet="10.66.66.0"):
+    """Генерирует новый IP-адрес, основываясь на указанной подсети."""
+    base_ip = ipaddress.IPv4Network(subnet, strict=False)
+    for ip in base_ip.hosts():
+        if str(ip) not in existing_ips:
+            return f"{ip}/32", str(ip)
+    raise RuntimeError("Нет доступных IP-адресов в подсети.")
 
-    # Генерируем новый IP, если нет доступных свободных
-    base_ipv4 = "10.66.66"
-    max_ipv4 = 1
-    for ip in existing_ips:
-        if ip.startswith(base_ipv4):
-            last_octet = int(ip.split('.')[-1])
-            if last_octet > max_ipv4:
-                max_ipv4 = last_octet
-    new_ipv4 = f"{base_ipv4}.{max_ipv4 + 1}"
-    ip_records[new_ipv4] = True  # Добавляем новый IP как занятый
-    save_ip_records(ip_records)
-    
-    return f"{new_ipv4}/32", new_ipv4
 
 def release_ip(ip):
     """
