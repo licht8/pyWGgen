ccf2f25c5aa9f962fad8cf4f174a0b46e260ecf6
Edd <33794654+giteed@users.noreply.github.com>
2024-11-22 23:18:09 +0300

    Update statistics_tab.py

--- gradio_admin/tabs/statistics_tab.py
+++ gradio_admin/tabs/statistics_tab.py
@@ -25,7 +25,7 @@ def statistics_tab():
             selected_user_info = gr.Textbox(
                 label="User Information", 
                 interactive=False, 
-                value="Please enter a query to filter user data and then click a cell to view user details and perform actions."
+                value="Use the search below and then click on a cell to view the details."
             )
 
         # Кнопки действий
