8fc19e3b8c49cbfbb4ab21e979e4357b80afb373
Edd <33794654+giteed@users.noreply.github.com>
2024-11-22 20:27:12 +0300

    Create delete_tab.py

---
+++ gradio_admin/tabs/delete_tab.py
@@ -0,0 +1,18 @@
+import gradio as gr
+
+def get_delete_tab():
+    """Создает вкладку для удаления пользователей."""
+    with gr.Tab("🔥 Delete"):
+        with gr.Row():
+            gr.Markdown("## Delete a user")
+        with gr.Column(scale=1, min_width=300):
+            delete_input = gr.Textbox(label="Username to delete", placeholder="Enter username...")
+            delete_button = gr.Button("Delete User")
+            delete_output = gr.Textbox(label="Result", interactive=False)
+
+            def handle_delete_user(username):
+                """Обработчик для удаления пользователя."""
+                from gradio_admin.delete_user import delete_user
+                return delete_user(username)
+
+            delete_button.click(handle_delete_user, inputs=delete_input, outputs=delete_output)
