86558eb437ed690515f7f26cff87e160c4256c21
Edd <33794654+giteed@users.noreply.github.com>
2024-11-22 05:58:45 +0300

    Update wg_interface.py

--- gradio_admin/wg_interface.py
+++ gradio_admin/wg_interface.py
@@ -48,9 +48,9 @@ def interface():
         
         show_inactive.change(fn=load_data, inputs=[show_inactive], outputs=[table])
 
-    app.launch()
+    return app
 
 
 if __name__ == "__main__":
-    interface()
+    admin_interface = interface()
     admin_interface.launch(server_name="0.0.0.0", server_port=7860, share=True)
