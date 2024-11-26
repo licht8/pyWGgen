a20355089fadd19d8b73af6ff442440285935ba1
Edd <33794654+giteed@users.noreply.github.com>
2024-11-26 05:02:31 +0300

    Update main_interface.py

--- gradio_admin/main_interface.py
+++ gradio_admin/main_interface.py
@@ -6,9 +6,9 @@ import os
 import sys
 import gradio as gr
 
-# Установка корректного пути для импорта модулей
+# Добавляем корневой путь в PYTHONPATH
 current_dir = os.path.dirname(os.path.abspath(__file__))
-project_root = os.path.abspath(os.path.join(current_dir, "../"))
+project_root = os.path.abspath(os.path.join(current_dir, ".."))
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
 
