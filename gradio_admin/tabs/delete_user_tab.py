1a5edaaa39b2c3860794b1dbeae7c02d514b9753
Edd <33794654+giteed@users.noreply.github.com>
2024-11-26 07:17:26 +0300

    Update delete_user_tab.py

--- gradio_admin/tabs/delete_user_tab.py
+++ gradio_admin/tabs/delete_user_tab.py
@@ -1,18 +1,25 @@
+#!/usr/bin/env python3
 # gradio_admin/tabs/delete_user_tab.py
+# Вкладка для удаления пользователей
 
 import gradio as gr
-from gradio_admin.functions.delete_user import delete_user  # Предполагаем, что эта функция уже реализована
+from gradio_admin.functions.delete_user import delete_user
 
 def delete_user_tab():
-    with gr.Row():
-        gr.Markdown("## Удалить пользователя")
-    with gr.Column(scale=1, min_width=300):
-        username_input = gr.Textbox(label="Имя пользователя", placeholder="Введите имя пользователя...")
-        delete_button = gr.Button("Удалить")
-        delete_output = gr.Textbox(label="Результат", interactive=False)
+    """
+    Вкладка для удаления пользователей WireGuard.
+    """
+    username_input = gr.Textbox(label="Имя пользователя", placeholder="Введите имя пользователя...")
+    delete_button = gr.Button("Удалить пользователя")
+    output_message = gr.Textbox(label="Результат", interactive=False)
 
-        delete_button.click(
-            delete_user,
-            inputs=username_input,
-            outputs=delete_output
-        )
+    def handle_delete_user(username):
+        return delete_user(username)
+
+    delete_button.click(
+        handle_delete_user,
+        inputs=username_input,
+        outputs=output_message
+    )
+    
+    return [username_input, delete_button, output_message]
