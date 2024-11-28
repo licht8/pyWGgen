67aba118397aec1ee4255427f210d29b1839cd42
Edd <33794654+giteed@users.noreply.github.com>
2024-11-28 21:45:41 +0300

    Update gradio_utils.py

--- modules/gradio_utils.py
+++ modules/gradio_utils.py
@@ -12,7 +12,7 @@ def run_gradio_admin_interface(port):
     handle_port_conflict(port)
     print(f"")
     open_firewalld_port(port)
-    print(f"\n🌐 Launching Gradio http://{get_external_ip()}:{port}")
+    print(f"\n🌐 Launching Gradio admin: http://{get_external_ip()}:{port}")
     admin_interface.launch(server_name="0.0.0.0", server_port=port, share=False)
     print(f"")
     close_firewalld_port(port)
