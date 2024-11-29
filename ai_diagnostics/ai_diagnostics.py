904058b5cbae1985f49d5129f0c494c26f53ce9e
Edd <33794654+giteed@users.noreply.github.com>
2024-11-29 03:25:17 +0300

    Update ai_diagnostics.py

--- ai_diagnostics/ai_diagnostics.py
+++ ai_diagnostics/ai_diagnostics.py
@@ -3,7 +3,7 @@ import time
 import sys
 from pathlib import Path
 
-# Добавляем корневую директорию в sys.path
+# Добавляем корневую директорию проекта в sys.path
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
 sys.path.append(str(PROJECT_ROOT))
 
