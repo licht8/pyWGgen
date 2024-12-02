f3c463e81d3dd49d7152ed00a2a46bda18788b92
Edd <33794654+giteed@users.noreply.github.com>
2024-12-02 21:07:47 +0300

    Update test_logging.py

--- modules/test_logging.py
+++ modules/test_logging.py
@@ -2,8 +2,16 @@
 # test_logging.py
 # Скрипт для проверки уровня логирования
 
+import sys
+from pathlib import Path
+
+# Добавляем корневую директорию проекта в sys.path
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+sys.path.append(str(PROJECT_ROOT))
+
+from settings import LOG_LEVEL, LOG_FILE_PATH  # Импорт настроек
+
 import logging
-from settings import LOG_LEVEL, LOG_FILE_PATH
 
 # Настраиваем logging
 logging.basicConfig(
