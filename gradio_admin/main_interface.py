82e588d7ccbd2120f34ce3636947c5332ad5eaa5
Edd <33794654+giteed@users.noreply.github.com>
2024-12-03 06:45:53 +0300

    Update main_interface.py

--- gradio_admin/main_interface.py
+++ gradio_admin/main_interface.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 # gradio_admin/main_interface.py
-# Основной интерфейс Gradio для управления проектом wg_qr_generator
 
 import gradio as gr
 from gradio_admin.tabs.create_user_tab import create_user_tab
 from gradio_admin.tabs.delete_user_tab import delete_user_tab
 from gradio_admin.tabs.statistics_tab import statistics_tab
-from gradio_admin.tabs.command_line_tab import command_line_tab  # Подключение новой вкладки
+from gradio_admin.tabs.command_line_tab import create_command_line_tab
 
 # Создание интерфейса
 with gr.Blocks() as admin_interface:
@@ -19,5 +18,5 @@ with gr.Blocks() as admin_interface:
     with gr.Tab(label="🔍 Статистика"):
         statistics_tab()
     
-    with gr.Tab(label="💻 Командная строка"):  # Добавляем вкладку
-        command_line_tab()
+    with gr.Tab(label="💻 Эмуляция CLI"):
+        create_command_line_tab()
