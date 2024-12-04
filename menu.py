53d881b072248e48996c17bc9e6e9787251a6ead
Edd <33794654+giteed@users.noreply.github.com>
2024-12-04 09:26:43 +0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -26,7 +26,7 @@ from modules.swap_edit import check_swap_edit, swap_edit
 from modules.swap_edit import check_swap_edit
 
 # Проверить и создать swap размером 64 MB, если необходимо
-check_swap_edit(size_mb=128, action="mini", silent=True)
+check_swap_edit(size_mb=64, action="micro", silent=True)
 
 
 # Установить путь к корню проекта
