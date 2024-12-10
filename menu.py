1f4ed4eb3e3b7a8fcc42796eef0f3132ab3817bc
Edd <33794654+giteed@users.noreply.github.com>
2024-12-10 17:57:47 +0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -106,10 +106,7 @@ def show_main_menu():
         if choice == "i":
             from modules.report_utils import display_summary_report, show_project_status
             display_summary_report()
-            #time.sleep(2)
             show_project_status()
-            #from modules.project_status import show_project_status
-            #show_project_status()
         elif choice == "t":
             print(f" 🔍  Запуск тестов...")
             subprocess.run(["pytest"])
