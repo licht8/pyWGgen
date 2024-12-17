b101f860200bcd8d16a6f44551c291c38d44ee6d
Edd <33794654+giteed@users.noreply.github.com>
2024-12-17 20:09:45 +0300

    Update main_interface.py

--- gradio_admin/main_interface.py
+++ gradio_admin/main_interface.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 # gradio_admin/main_interface.py
-# Основной интерфейс Gradio для управления проектом wg_qr_generator
 
 import gradio as gr
 from gradio_admin.tabs.create_user_tab import create_user_tab
 from gradio_admin.tabs.delete_user_tab import delete_user_tab
 from gradio_admin.tabs.statistics_tab import statistics_tab
-from gradio_admin.tabs.command_line_tab import command_line_tab  # Подключение новой вкладки
+from gradio_admin.tabs.ollama_chat_tab import ollama_chat_tab  # Новый импорт
 
 # Создание интерфейса
 with gr.Blocks() as admin_interface:
@@ -18,6 +17,10 @@ with gr.Blocks() as admin_interface:
     
     with gr.Tab(label="🔍 Статистика"):
         statistics_tab()
+    
+    with gr.Tab(label="🤖 Чат с Ollama"):
+        ollama_chat_tab()
+
     
     #with gr.Tab(label="🖥️ Командная строка"):
         #command_line_tab()
